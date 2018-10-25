a782e7898576bfe5c7f3cdec7dcf6c2cb9e5cde2
phothet <phothet@gmail.com>
2018-10-24 22:50:59 -0400

    Create matt_genkey.sh

---
+++ matt_genkey.sh
@@ -0,0 +1,61 @@
+[matt@zabbix1 ~]$ gpg --gen-key
+gpg (GnuPG) 2.0.22; Copyright (C) 2013 Free Software Foundation, Inc.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+gpg: directory `/home/matt/.gnupg' created
+gpg: new configuration file `/home/matt/.gnupg/gpg.conf' created
+gpg: WARNING: options in `/home/matt/.gnupg/gpg.conf' are not yet active during this run
+gpg: keyring `/home/matt/.gnupg/secring.gpg' created
+gpg: keyring `/home/matt/.gnupg/pubring.gpg' created
+Please select what kind of key you want:
+   (1) RSA and RSA (default)
+   (2) DSA and Elgamal
+   (3) DSA (sign only)
+   (4) RSA (sign only)
+Your selection? 
+RSA keys may be between 1024 and 4096 bits long.
+What keysize do you want? (2048) 4096
+Requested keysize is 4096 bits
+Please specify how long the key should be valid.
+         0 = key does not expire
+      <n>  = key expires in n days
+      <n>w = key expires in n weeks
+      <n>m = key expires in n months
+      <n>y = key expires in n years
+Key is valid for? (0) 
+Key does not expire at all
+Is this correct? (y/N) y
+
+GnuPG needs to construct a user ID to identify your key.
+
+Real name: mattg
+Email address: 
+Comment: 
+You selected this USER-ID:
+    "mattg"
+
+Change (N)ame, (C)omment, (E)mail or (O)kay/(Q)uit? O
+You need a Passphrase to protect your secret key.
+
+We need to generate a lot of random bytes. It is a good idea to perform
+some other action (type on the keyboard, move the mouse, utilize the
+disks) during the prime generation; this gives the random number
+generator a better chance to gain enough entropy.
+We need to generate a lot of random bytes. It is a good idea to perform
+some other action (type on the keyboard, move the mouse, utilize the
+disks) during the prime generation; this gives the random number
+generator a better chance to gain enough entropy.
+gpg: /home/matt/.gnupg/trustdb.gpg: trustdb created
+gpg: key DF117D4C marked as ultimately trusted
+public and secret key created and signed.
+
+gpg: checking the trustdb
+gpg: 3 marginal(s) needed, 1 complete(s) needed, PGP trust model
+gpg: depth: 0  valid:   1  signed:   0  trust: 0-, 0q, 0n, 0m, 0f, 1u
+pub   4096R/DF117D4C 2018-10-25
+      Key fingerprint = 2182 5982 C5DE F761 8F4D  1D78 EEEC 6B63 DF11 7D4C
+uid                  mattg
+sub   4096R/F7FD4364 2018-10-25
+
+[matt@zabbix1 ~]$ 

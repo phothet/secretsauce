82d4ff3aaed670b2432814ed6e039761efe0d1b2
phothet <phothet@gmail.com>
2018-10-24 22:23:50 -0400

    Update bigsauce.sh

--- bigsauce.sh
+++ bigsauce.sh
@@ -1,10 +1,25 @@
 #secretsauce recipe with gnupg gpg
 
+# https://yanhan.github.io/posts/2014-03-04-gpg-how-to-trust-imported-key.html
+# http://blog.ghostinthemachines.com/2015/03/01/how-to-use-gpg-command-line/
+# https://www.lifewire.com/wipe-vs-shred-vs-delete-vs-erase-whats-the-difference-2619146
+
+#without screen running, we can't enter password
 yum -y install screen
+
+#generate gpg key
+#chooe 4096
+#if slow, it's good to install haveaged or some sort
 gpg --gen-key
 
 #type in username (not firstname last name. just username)
 #type in password
+
+#export your public key for others in ascii mode
+gpg -a --export > mypubkey.asc
+
+#and import other keys
+gpg --import jimkey.asc joekey.asc
 
 #retrieve pub key by running 
 gpg --list-key
